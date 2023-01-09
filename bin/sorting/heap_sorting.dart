3303bff713b48f0a8c5935e3f91d0d855cd89934
aswinsubhash <aswinsubhash3@gmail.com>
2023-01-09 16:48:36 +0530

    heap and quick sort completed

--- bin/sorting/heap_sorting.dart
+++ bin/sorting/heap_sorting.dart
@@ -1,44 +1,57 @@
-void main() {
-  List<int> numbers = [3, 7, 4, 9, 5, 2, 6, 1];
 
+
+// Time complexity of heap sort is O(n log(n)) in all the  cases//
+
+
+void main() { 
+  // Create a list of integers
+  List<int> numbers = [4,11,2,7,1];
+
+  // Sort the list using the heapSort function
   heapSort(numbers);
 
-  print(numbers); // [1, 2, 3, 4, 5, 6, 7, 9]
+  // Print the sorted list to the console
+  print(numbers); 
 }
 
+// Definition of the heapSort function
 void heapSort(List<int> numbers) {
-  // Build max heap
+  // Build a max heap from the list of numbers
   for (int i = (numbers.length / 2).floor() - 1; i >= 0; i--) {
+    // heapify the subtree rooted at index i
     heapify(numbers, numbers.length, i);
   }
 
-  // Extract elements from the heap one by one
-  for (int i = numbers.length - 1; i >= 0; i--) {
-    // Move current root to the end
+  // Extract the elements of the heap one by one, building the sorted list
+  for (int i = numbers.length - 1; i >= 0; i--) { 
+    // Move the current root to the end of the list
     int temp = numbers[0];
     numbers[0] = numbers[i];
     numbers[i] = temp;
 
-    // Call max heapify on the reduced heap
+    // Call heapify on the reduced heap to restore the max heap property
     heapify(numbers, i, 0);
   }
 }
 
+// Definition of the heapify function
 void heapify(List<int> numbers, int n, int i) {
-  // Find largest among root, left child and right child
+  // Find the largest value among the root, left child, and right child
   int largest = i;
-  int l = 2 * i + 1;
-  int r = 2 * i + 2;
+  int l = 2 * i + 1; // index of the left child
+  int r = 2 * i + 2; // index of the right child
 
   if (l < n && numbers[l] > numbers[largest]) {
+    // If the left child is larger than the root, update largest
     largest = l;
   }
 
   if (r < n && numbers[r] > numbers[largest]) {
+    // If the right child is larger than the root, update largest
     largest = r;
   }
 
-  // Swap and continue heapifying if root is not largest
+  // If the root is not the largest, swap it with the largest value and continue heapifying
   if (largest != i) {
     int temp = numbers[i];
     numbers[i] = numbers[largest];
--- bin/sorting/quick_sorting.dart
+++ bin/sorting/quick_sorting.dart
@@ -1,36 +1,57 @@
+
+ // Timecomplexity of quicksort in best and worst case is O(n log(n)) and in worst case its O(n^2)//
+
+
 void main() {
+  // Declare a list of integers
   List<int> numbers = [3, 7, 4, 9, 5, 2, 6, 1];
 
+  // Sort the list using the quick sort algorithm
   quickSort(numbers, 0, numbers.length - 1);
 
-  print(numbers); // [1, 2, 3, 4, 5, 6, 7, 9]
+  // Print the sorted list
+  print(numbers); 
 }
 
+// The quick sort function that sorts the list between the low and high indices
 void quickSort(List<int> numbers, int low, int high) {
+  // If the low index is less than the high index, there is a possibility to partition the list
   if (low < high) {
-    int pi = partition(numbers, low, high);
+    // Get the pivot index by partitioning the list between the low and high indices
+    int pivotIndex = partition(numbers, low, high);
 
-    quickSort(numbers, low, pi - 1);
-    quickSort(numbers, pi + 1, high);
+    // Recursively sort the list before the pivot index
+    quickSort(numbers, low, pivotIndex - 1);
+    // Recursively sort the list after the pivot index
+    quickSort(numbers, pivotIndex + 1, high);
   }
 }
 
+// The partition function that finds the pivot index for the quick sort algorithm
 int partition(List<int> numbers, int low, int high) {
+  // Select the last element as the pivot
   int pivot = numbers[high];
-  int i = low - 1;
+  // Initialize the index of the "smaller" element
+  int smallerIndex = low - 1;
 
-  for (int j = low; j < high; j++) {
-    if (numbers[j] < pivot) {
-      i++;
-      int temp = numbers[i];
-      numbers[i] = numbers[j];
-      numbers[j] = temp;
+  // Iterate through the list between the low and high indices
+  for (int currentIndex = low; currentIndex < high; currentIndex++) {
+    // If the current element is smaller than the pivot
+    if (numbers[currentIndex] < pivot) {
+      // Increment the index of the "smaller" element
+      smallerIndex++;
+      // Swap the current element with the element at the "smaller" index
+      int temp = numbers[smallerIndex];
+      numbers[smallerIndex] = numbers[currentIndex];
+      numbers[currentIndex] = temp;
     }
   }
 
-  int temp = numbers[i + 1];
-  numbers[i + 1] = numbers[high];
+  // Swap the pivot element with the element at the "smaller" index + 1
+  int temp = numbers[smallerIndex + 1];
+  numbers[smallerIndex + 1] = numbers[high];
   numbers[high] = temp;
 
-  return i + 1;
+  // Return the pivot index
+  return smallerIndex + 1;
 }
