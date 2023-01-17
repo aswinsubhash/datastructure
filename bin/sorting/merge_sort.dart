5da6eaea777b8f4afe2ec49e68c06429d99bc7cd
aswinsubhash <aswinsubhash3@gmail.com>
2023-01-17 14:35:37 +0530

    merge sort added

--- bin/sorting/merge_sort.dart
+++ bin/sorting/merge_sort.dart
@@ -1,17 +1,20 @@
-void mergeSort(List<int> list) {
-  // Check if the list has a length less than or equal to 1
-  if (list.length <= 1) return;
 
+int mergeSort(List<int> list, int count) {
+  if (list.length <= 1) return count;
+  
+  count++; //increment the counter
+  
   // Get the middle index of the list
   var middle = list.length ~/ 2;
 
   // Split the list into left and right sublists
   var left = list.sublist(0, middle);
   var right = list.sublist(middle);
-
+print(left);
+print(right); 
   // Recursively sort the left and right sublists
-  mergeSort(left);
-  mergeSort(right);
+  count = mergeSort(left, count);
+  count = mergeSort(right, count);
 
   // Initialize variables for merging the sublists back together
   var i = 0;
@@ -48,37 +51,13 @@ void mergeSort(List<int> list) {
     j++;
     k++;
   }
-}
-
-int countListDivisions(List<int> list) {
-  // Initialize a count variable to keep track of the number of divisions
-  int count = 0;
-
-  // While the length of the list is greater than 1
-  while (list.length > 1) {
-    // Get the middle index of the list
-    int middle = list.length ~/ 2;
-
-    // Divide the list into two equal sublists
-    list = list.sublist(middle);
-
-    // Increment the count for each division
-    count++;
-  }
-
-  // Return the final count of divisions
   return count;
 }
 
-
-
 void main() {
-  // initialize list to be sorted
+  int count = 0;
   List<int> list = [7,8,1,5,2,9];
-  // sort the list
-  mergeSort(list);
-  //print the sorted list
+  count = mergeSort(list, count);
   print(list);
-  print(countListDivisions(list)); 
-
+  print(count);
 }
